a3d23cec1917e4e92dbdfe5697eeb60811625c2e
Seun Shola <ooluwasseun123@gmail.com>
2026-05-06 15:13:01 +0100

    final push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   const navLinks = [
     { name: "Home", path: "/" },
-    { name: "Training Courses", path: "/sholztech/AllCourses" },
+
     {
       name: "About Us",
       path: "/about",
